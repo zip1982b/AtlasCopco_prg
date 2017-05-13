ace3f7426be6689d2c698bf7bc3eef26da49e1e7
zip1982b <zip1982b@gmail.com>
2017-05-13 20:12:59 +0200

    work PC

--- test_class.py
+++ test_class.py
@@ -14,6 +14,21 @@ class SecondClass(FirstClass):      #наследует set_data
         print('current value = "%s"' % self.data)
 
 """Иногда такая замена атрибутов за счет их переопределения ниже в дереве классов называется перегрузкой."""
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+    def __str__(self):
+        return '[ThirdClass: %s]' % self.data
+    def mul(self, other):
+        self.data *= other
+
+
+class Test:
+    pass
+
 
 
 if __name__ == '__main__':
@@ -21,18 +36,37 @@ if __name__ == '__main__':
     y = FirstClass()        #создаются объекты экземпляров (из объекта класса FirstClass), а x и y это ссылки на них
                             #на данный момент имеются три связаных пространства имён
                             #атрибут data обнаруживается в объекте экземпляра, а set_data и display в классе
-    z = SecondClass()
-    z.set_data(555)
-    z.display()
+    #z = ThirdClass(55)
+    #z.set_data(55)
+    #z.display()
+    #print(z)
+    Test.name = "bob"
+    Test.age = 40
+    print(Test.age, Test.name)
+
+    z = Test()
+    a = Test()
+    z.name="ava"
+    print(z.name, Test.name)
+    print(dir(ThirdClass))
+    print(ThirdClass.__dict__.keys())
+    print(Test.__dict__.keys())
+    print(z.__dict__.keys())
+    print(a.__dict__.keys())
+    print(a.__class__)
+    print(z.__class__)
+    #z = SecondClass()
+    #z.set_data(555)
+    #z.display()
 
 
     #FirstClass.set_data(y, "Hi")
     #FirstClass.display(y)
 
-    x.set_data(5)
-    x.display()
-    x.data = "Hi"
-    x.display()
+    #x.set_data(5)
+    #x.display()
+    #x.data = "Hi"
+    #x.display()
     #print(x)
     #print(FirstClass)
 
