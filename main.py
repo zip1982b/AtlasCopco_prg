d9c71295547c7816be4fb615c1a49ea9074aea60
zip1982b <zip1982b@gmail.com>
2019-03-02 20:09:46 +0200

    add exception

--- main.py
+++ main.py
@@ -7,11 +7,29 @@ from assemblyMessage_v2 import message, assembly_header, MID
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '192.168.8.1'
 port = 4545
-s.connect((host, port))
+try:
+    s.connect((host, port))
+    print("connected")
+    s.send(message(assembly_header(MID['Communication_start'])).encode())
+
+    data = s.recv(1000)
+    dataASCII = data.decode("ascii")
+    print('recivied:', dataASCII, len(data), 'bytes')
+
+    s.close()
+
+except ConnectionRefusedError:
+    print("connection error")
+    print("connection again ... ")
+    try:
+        s.connect((host, port))
+    except ConnectionRefusedError:
+        print("connection error 2")
 
 
 
 
+print("end prg ...... ")
 
 
 
@@ -20,11 +38,4 @@ s.connect((host, port))
 # а может быть нужно добовлять Message end - nul,
 # NUL необходимо добавлять к сообщению
 
-s.send(message(assembly_header(MID['Communication_start'])).encode())
-
-data = s.recv(1000)
-dataASCII=data.decode("ascii")
-print ('recivied:', dataASCII, len(data), 'bytes')
-
-s.close()
 
