0b18700f590ffe627625acd02bc0a7307e8518a6
zip1982b <zip1982b@gmail.com>
2019-03-03 19:24:11 +0200

    count connection 3

--- main.py
+++ main.py
@@ -3,39 +3,36 @@ import sys
 from assemblyMessage_v2 import message, assembly_header, MID
 
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '192.168.8.1'
-port = 4545
-try:
+def work_PF4000():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '192.168.8.1'
+    port = 4545
     s.connect((host, port))
-    print("connected")
+    print("connected on TCP")
     s.send(message(assembly_header(MID['Communication_start'])).encode())
 
     data = s.recv(1000)
     dataASCII = data.decode("ascii")
     print('recivied:', dataASCII, len(data), 'bytes')
-
+    print("close socket - disconected ...")
     s.close()
 
-except ConnectionRefusedError:
-    print("connection error")
-    print("connection again ... ")
+
+
+def connecting_again():
     try:
-        s.connect((host, port))
-    except ConnectionRefusedError:
-        print("connection error 2")
+        work_PF4000()
+    except Exception as ex:
+        template = "An exeption of type {0} occured. Arguments: \n{1!r}"
+        mes = template.format(type(ex).__name__, ex.args)
+        print(mes)
 
 
+
+count = 3
+while count > 0:
+    connecting_again()
+    count = count - 1
 
 
 print("end prg ...... ")
-
-
-
-#sendData = b'002000010000000000000' #количество байт для передачи должно быть больше на 1
-# Python скорее всего считает на 1 больше
-# а может быть нужно добовлять Message end - nul,
-# NUL необходимо добавлять к сообщению
-
-
